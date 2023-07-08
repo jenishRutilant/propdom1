e5f86adfcecaafa1b7014c66aaad133918f2a79c
Jenish <jenish.rutilant@gmail.com>
2023-07-08 16:49:46 +0530

    sd

--- src/Pages/ImageSlider.jsx
+++ src/Pages/ImageSlider.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import "../Style/Home.css"
 import Aos from 'aos';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.min.css';
-import 'swiper/components/pagination/pagination.min.css';
-
 
 const ImageSlider = ({ images, autoplayInterval = 3000 }) => {
     console.log(images);
     const [currentImage] = useState(0);
- 
+    // setCurrentImage
+    // const handlePrev = () => {
+    //     setCurrentImage((prevImage) => (prevImage === 0 ? images.length - 1 : prevImage - 1));
+    // };
+
+    // const handleNext = () => {
+    //     setCurrentImage((prevImage) => (prevImage === images.length - 1 ? 0 : prevImage + 1));
+    // };
+
     useEffect(() => {
         Aos.init({});
 
@@ -26,29 +30,19 @@ const ImageSlider = ({ images, autoplayInterval = 3000 }) => {
         <div className="image-slider">
             {/* <button className='handlePrev' onClick={handlePrev}>&lt;</button> */}
             <div className="slider-part">
-                <Swiper
-                    spaceBetween={30}
-                    slidesPerView={1}
-                    pagination={{ clickable: true }}
-                >
-                    {images?.map((item, i) => (
-                        <SwiperSlide key={i}>
-                            <div className='allSlider'>
-                                <img
-                                    data-aos="fade-left"
-                                    data-aos-delay="500"
-                                    className='slider-img'
-                                    src={item.img}
-                                    alt={`${i + 1}`}
-                                />
+                    {
+                        images?.map((item, i) => (
+                            <>
+                            <div>
+                                <img data-aos="fade-left" data-aos-delay="500" className='slider-img' src={item.img} alt={`${currentImage + 1}`} />
                                 <div className="dummy-tag" data-aos="fade-left" data-aos-delay="500">
                                     <h3>{item.title}</h3>
                                     <span style={{ color: "white" }}>{item.desc}</span>
                                 </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+                                </div>
+                            </>
+                        ))
+                    }
             </div>
             {/* <button onClick={handleNext} className='handlePrev'>&gt;</button> */}
         </div>
